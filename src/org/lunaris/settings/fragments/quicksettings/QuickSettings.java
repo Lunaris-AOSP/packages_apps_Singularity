410d2a40436dc61424eb0b980cdf40abe33fbe20
Ghosuto <clash.raja10@gmail.com>
2025-07-14 04:14:04 +0000

    Init Singularity
    - Full credit goes to respective authors and projects mentioned in org readme
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Joey <joey@evolution-x.org>
    Co-authored-by: someone5678 <59456192+someone5678@users.noreply.github.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Co-authored-by: darkphnx <dark.phnx12@gmail.com>
    Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Co-authored-by: Dmitrii <bankersenator@gmail.com>
    Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Change-Id: Ica98d4f11010152777308cdd1b90114416acd7d4

---
+++ src/org/lunaris/settings/fragments/quicksettings/QuickSettings.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2017-2024 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lunaris.settings.fragments.quicksettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.SearchIndexable;
+
+import org.lunaris.settings.utils.DeviceUtils;
+
+import java.util.List;
+
+@SearchIndexable
+public class QuickSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "QuickSettings";
+
+    private static final String KEY_INTERFACE_CATEGORY = "quick_settings_interface_category";
+    private static final String KEY_MISCELLANEOUS_CATEGORY = "quick_settings_miscellaneous_category";
+    private static final String KEY_QS_BLUETOOTH_SHOW_DIALOG = "qs_bt_show_dialog";
+
+    private PreferenceCategory mInterfaceCategory;
+    private PreferenceCategory mMiscellaneousCategory;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.lunaris_settings_quick_settings);
+
+        final Context mContext = getContext();
+        final ContentResolver resolver = mContext.getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources resources = mContext.getResources();
+
+        mMiscellaneousCategory = (PreferenceCategory) findPreference(KEY_MISCELLANEOUS_CATEGORY);
+
+        if (!DeviceUtils.deviceSupportsBluetooth(mContext)) {
+            prefScreen.removePreference(mMiscellaneousCategory);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.LUNARIS;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider(R.xml.lunaris_settings_quick_settings) {
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                final Resources resources = context.getResources();
+
+                if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+                    keys.add(KEY_QS_BLUETOOTH_SHOW_DIALOG);
+                }
+                return keys;
+            }
+        };
+}
