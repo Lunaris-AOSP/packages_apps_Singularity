Mist-OS-Staging/packages_apps_Mistify

add some wifi icons style
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

src/org/mist/settings/fragments/themes/Themes.java
@@ -41,6 +41,9 @@ import org.mist.settings.utils.SystemUtils;
 import org.mist.settings.preferences.SystemSettingListPreference;
 
 import com.android.internal.util.mist.VibrationUtils;
+import com.android.internal.util.mist.ThemeUtils;
+
+import org.mist.settings.utils.SystemRestartUtils;
 
 import java.util.List;
 
@@ -53,10 +56,21 @@ public class Themes extends SettingsPreferenceFragment implements
     private static final String KEY_FORCE_FULL_SCREEN = "display_cutout_force_fullscreen_settings";
     private static final String SMART_PIXELS = "smart_pixels";
     private static final String KEY_VOLUME_DIALOG_TYPE = "volume_dialog_type";
+    private static final String KEY_WIFI_ICON_STYLE = "wifi_icon_style";
 
     private Preference mShowCutoutForce;
     private Preference mSmartPixels;
     private SystemSettingListPreference mVolumeDialogType;
+    private SystemSettingListPreference mWifiIconStyle;
+    private ThemeUtils mThemeUtils;
+
+    private static final String[] WIFI_ICON_OVERLAYS = {
+            "com.custom.overlay.systemui.wifiAurora",
+            "com.android.systemui.wifibar_c",
+            "com.custom.overlay.systemui.wifiLinear",
+            "com.android.systemui.wifiNothingDot",
+            "com.android.systemui.wifibar_d"
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,7 +81,9 @@ public class Themes extends SettingsPreferenceFragment implements
         Context mContext = getActivity().getApplicationContext();
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
-	    final String displayCutout =
+        mThemeUtils = ThemeUtils.getInstance(getActivity());
+
+        final String displayCutout =
             mContext.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
 
         if (TextUtils.isEmpty(displayCutout)) {
@@ -85,14 +101,57 @@ public class Themes extends SettingsPreferenceFragment implements
         if (mVolumeDialogType != null) {
             mVolumeDialogType.setOnPreferenceChangeListener(this);
         }
+
+        mWifiIconStyle = findPreference(KEY_WIFI_ICON_STYLE);
+        if (mWifiIconStyle != null) {
+            mWifiIconStyle.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    private void updateStyle(String key, String category, String target,
+            int defaultValue, String[] overlayPackages, boolean restartSystemUI) {
+        final int style = Settings.System.getIntForUser(
+                getContext().getContentResolver(),
+                key,
+                defaultValue,
+                UserHandle.USER_CURRENT
+        );
+        if (mThemeUtils == null) {
+            mThemeUtils = ThemeUtils.getInstance(getContext());
+        }
+        mThemeUtils.setOverlayEnabled(category, target, target);
+        if (style > 0 && style <= overlayPackages.length) {
+            mThemeUtils.setOverlayEnabled(category, overlayPackages[style - 1], target);
+        }
+        if (restartSystemUI) {
+            SystemRestartUtils.restartSystemUI(getContext());
+        }
+    }
+
+    private void updateWifiIconStyle() {
+        updateStyle(KEY_WIFI_ICON_STYLE, "android.theme.customization.wifi_icon", 
+                "com.android.systemui", 0, WIFI_ICON_OVERLAYS, true);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        int value = 0;
+        
         if (preference == mVolumeDialogType) {
             SystemUtils.showSystemUiRestartDialog(getActivity());
             return true;
         }
+        
+        if (preference == mWifiIconStyle) {
+            value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    KEY_WIFI_ICON_STYLE, value, UserHandle.USER_CURRENT);
+            updateWifiIconStyle();
+            return true;
+        }
+        
         return false;
     }
 
@@ -116,7 +175,7 @@ public class Themes extends SettingsPreferenceFragment implements
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
 
-	                final String displayCutout =
+                    final String displayCutout =
                         context.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
 
                     if (TextUtils.isEmpty(displayCutout)) {

Lunaris-AOSP/packages_apps_Singularity

src/org/lunaris/settings/fragments/themes/Themes.java
@@ -41,6 +41,9 @@ import org.lunaris.settings.utils.SystemUtils;
 import org.lunaris.settings.preferences.SystemSettingListPreference;
 
 import com.android.internal.util.lunaris.VibrationUtils;
+import com.android.internal.util.lunaris.ThemeUtils;
+
+import org.lunaris.settings.utils.SystemRestartUtils;
 
 import java.util.List;
 
@@ -53,10 +56,21 @@ public class Themes extends SettingsPreferenceFragment implements
     private static final String KEY_FORCE_FULL_SCREEN = "display_cutout_force_fullscreen_settings";
     private static final String SMART_PIXELS = "smart_pixels";
     private static final String KEY_VOLUME_DIALOG_TYPE = "volume_dialog_type";
+    private static final String KEY_WIFI_ICON_STYLE = "wifi_icon_style";
 
     private Preference mShowCutoutForce;
     private Preference mSmartPixels;
     private SystemSettingListPreference mVolumeDialogType;
+    private SystemSettingListPreference mWifiIconStyle;
+    private ThemeUtils mThemeUtils;
+
+    private static final String[] WIFI_ICON_OVERLAYS = {
+            "com.custom.overlay.systemui.wifiAurora",
+            "com.android.systemui.wifibar_c",
+            "com.custom.overlay.systemui.wifiLinear",
+            "com.android.systemui.wifiNothingDot",
+            "com.android.systemui.wifibar_d"
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,7 +81,9 @@ public class Themes extends SettingsPreferenceFragment implements
         Context mContext = getActivity().getApplicationContext();
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
-	    final String displayCutout =
+        mThemeUtils = ThemeUtils.getInstance(getActivity());
+
+        final String displayCutout =
             mContext.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
 
         if (TextUtils.isEmpty(displayCutout)) {
@@ -85,14 +101,57 @@ public class Themes extends SettingsPreferenceFragment implements
         if (mVolumeDialogType != null) {
             mVolumeDialogType.setOnPreferenceChangeListener(this);
         }
+
+        mWifiIconStyle = findPreference(KEY_WIFI_ICON_STYLE);
+        if (mWifiIconStyle != null) {
+            mWifiIconStyle.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    private void updateStyle(String key, String category, String target,
+            int defaultValue, String[] overlayPackages, boolean restartSystemUI) {
+        final int style = Settings.System.getIntForUser(
+                getContext().getContentResolver(),
+                key,
+                defaultValue,
+                UserHandle.USER_CURRENT
+        );
+        if (mThemeUtils == null) {
+            mThemeUtils = ThemeUtils.getInstance(getContext());
+        }
+        mThemeUtils.setOverlayEnabled(category, target, target);
+        if (style > 0 && style <= overlayPackages.length) {
+            mThemeUtils.setOverlayEnabled(category, overlayPackages[style - 1], target);
+        }
+        if (restartSystemUI) {
+            SystemRestartUtils.restartSystemUI(getContext());
+        }
+    }
+
+    private void updateWifiIconStyle() {
+        updateStyle(KEY_WIFI_ICON_STYLE, "android.theme.customization.wifi_icon", 
+                "com.android.systemui", 0, WIFI_ICON_OVERLAYS, true);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        int value = 0;
+        
         if (preference == mVolumeDialogType) {
             SystemUtils.showSystemUiRestartDialog(getActivity());
             return true;
         }
+        
+        if (preference == mWifiIconStyle) {
+            value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    KEY_WIFI_ICON_STYLE, value, UserHandle.USER_CURRENT);
+            updateWifiIconStyle();
+            return true;
+        }
+        
         return false;
     }
 
@@ -116,7 +175,7 @@ public class Themes extends SettingsPreferenceFragment implements
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
 
-	                final String displayCutout =
+                    final String displayCutout =
                         context.getResources().getString(com.android.internal.R.string.config_mainBuiltInDisplayCutout);
 
                     if (TextUtils.isEmpty(displayCutout)) {